0a8002002b4fa6059e528dfd2fe5eed65fa438f2
Andre Nicholas Jr <pumpgaming2@gmail.com>
2026-05-11 12:05:38 -0500

    Refactor VideoRenderingSubState constructor options

--- source/options/VideoRenderingSubState.hx
+++ source/options/VideoRenderingSubState.hx
@@ -1,55 +1,49 @@
 package options;
 
-import options.BaseOptionsMenu;
-import options.Option;
-
 class VideoRenderingSubState extends BaseOptionsMenu
 {
-    public function new()
-    {
-        title = "Video Rendering";
-        rpcTitle = "Video Rendering Menu";
+	public function new()
+	{
+		title = "Video Rendering";
+		rpcTitle = "Video Rendering Menu";
 
-        // Enable Renderer
-        var option:Option = new Option(
-            "Game Renderer",
-            "Records gameplay using FFmpeg.\nRequires FFmpeg installed on system PATH.",
-            "gameRenderer",
-            BOOL
-        );
-        addOption(option);
+		var option:Option = new Option(
+			'Game Renderer',
+			"Records gameplay using FFmpeg.\nRequires FFmpeg installed on system PATH.",
+			'gameRenderer',
+			BOOL
+		);
+		addOption(option);
 
-        // Recording FPS
-        option = new Option(
-            "Record FPS",
-            "Sets the FPS used for recording output.",
-            "videoRenderFPS",
-            INT
-        );
-        option.minValue = 30;
-        option.maxValue = 240;
-        option.changeValue = 1;
-        option.decimals = 0;
-        addOption(option);
+		option = new Option(
+			'Record FPS',
+			"Sets the FPS used for recording output.",
+			'videoRenderFPS',
+			INT
+		);
+		option.minValue = 30;
+		option.maxValue = 240;
+		option.changeValue = 1;
+		option.decimals = 0;
+		addOption(option);
 
-        // Quality
-        option = new Option(
-            "Recording Quality",
-            "Higher values look better but create larger files.",
-            "videoRenderQuality",
-            INT
-        );
-        option.minValue = 1;
-        option.maxValue = 10;
-        option.changeValue = 1;
-        option.decimals = 0;
-        addOption(option);
+		option = new Option(
+			'Recording Quality',
+			"Higher values look better but create larger files.",
+			'videoRenderQuality',
+			INT
+		);
+		option.minValue = 1;
+		option.maxValue = 10;
+		option.changeValue = 1;
+		option.decimals = 0;
+		addOption(option);
 
-        super();
-    }
+		super();
+	}
 
-    override function update(elapsed:Float)
-    {
-        super.update(elapsed);
-    }
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
 }
